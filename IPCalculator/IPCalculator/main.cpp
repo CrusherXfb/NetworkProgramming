a1be5c93924547e78a9808a321d01ef2a1e7f31e
Crusher <CrusherXfb@outlook.com>
2024-06-10 11:46:03 +0300

    network_information IN_PROGRESS

--- IPCalculator/IPCalculator/main.cpp
+++ IPCalculator/IPCalculator/main.cpp
@@ -13,7 +13,19 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	return 0;
 }
 
-BOOL CheckMask(DWORD mask)
+//CHAR* ParseAddress(CHAR sz_address[], CHAR sz_desctiprion[], DWORD dw_address)
+//{
+//	sprintf(
+//		sz_address,
+//		sz_desctiprion,
+//		FIRST_IPADDRESS(dw_address),
+//		SECOND_IPADDRESS(dw_address),
+//		THIRD_IPADDRESS(dw_address),
+//		FOURTH_IPADDRESS(dw_address)
+//	);
+//}
+
+/*BOOL CheckMask(DWORD mask)
 {
 	DWORD pattern = 1 << 32;
 	for (int i = 0; i < 32; i++)
@@ -37,7 +49,7 @@ INT CountOnes(DWORD mask)
 		}
 	}
 	return 32 - zero_bits;
-}
+}*/
 
 //Процедура окна - это самая обычная функция, которая вызывается при запуске окна
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -119,20 +131,69 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		break;
 		case IDC_EDIT_PREFIX:
 		{
+			HWND hIPAddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 			HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
 			HWND hIpMask = GetDlgItem(hwnd, IDC_IPMASK);
+			HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
 			DWORD dw_mask = ~0;
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
-				CONST INT SIZE = 8;
-				CHAR sz_buffer[SIZE];
-				SendMessage(hEditPrefix, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				CONST INT SIZE_PREFIX = 8;
+				CHAR sz_buffer[SIZE_PREFIX];
+				SendMessage(hEditPrefix, WM_GETTEXT, SIZE_PREFIX, (LPARAM)sz_buffer);
 				//MessageBox(hwnd, sz_buffer, "Prefix", MB_OK | MB_ICONINFORMATION);
 				INT shift = atoi(sz_buffer);
 				//dw_mask >>= (32 - shift); //Функция atoi() преобразует ASCII-строку в значение типа 'int'
 				//dw_mask <<= (32 - shift);
 				//SendMessage(hIpMask, IPM_SETADDRESS, 0, (LPARAM)dw_mask);
-				SendMessage(hIpMask, IPM_SETADDRESS, 0, dw_mask >> (32 - shift) << (32 - shift));
+				SendMessage(hIpMask, IPM_SETADDRESS, 0, dw_mask /*>> (32 - shift)*/ <<= (32 - shift));
+
+
+
+
+
+				CONST INT SIZE = 256;
+				CHAR sz_info[SIZE]{};
+				CHAR sz_network_address[SIZE]{};
+				CHAR sz_broadcast_address[SIZE]{};
+				CHAR sz_number_of_hosts[SIZE]{};
+				DWORD dw_address = 0;
+				SendMessage(hIPAddress, IPM_GETADDRESS, 0, (LPARAM)&dw_address);
+				DWORD dw_network_address = dw_address & dw_mask;
+
+
+
+				sprintf(
+					sz_network_address, 
+					"Адрес сети:\t\t\t%i.%i.%i.%i", 
+					FIRST_IPADDRESS(dw_network_address), 
+					SECOND_IPADDRESS(dw_network_address), 
+					THIRD_IPADDRESS(dw_network_address), 
+					FOURTH_IPADDRESS(dw_network_address)
+				);
+
+				DWORD dw_broadcast_address = ~dw_mask | dw_network_address;
+				sprintf(
+					sz_broadcast_address,
+					"Широковещательный адрес:\t%i.%i.%i.%i",
+					FIRST_IPADDRESS(dw_broadcast_address),
+					SECOND_IPADDRESS(dw_broadcast_address),
+					THIRD_IPADDRESS(dw_broadcast_address),
+					FOURTH_IPADDRESS(dw_broadcast_address)
+				);
+
+				DWORD dw_number_of_hosts = dw_broadcast_address - dw_network_address - 1;
+				sprintf(sz_number_of_hosts, "Количество узлов:\t\t%i", dw_number_of_hosts);
+
+				sprintf(
+					sz_info,
+					"Info:\n%s\n%s\n%s",
+					sz_network_address,
+					sz_broadcast_address,
+					sz_number_of_hosts
+				);
+				SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)sz_info);
+
 			}
 		}
 		break;
